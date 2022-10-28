ce6d57df0e72081e04840bc5cf37b871bb879943
mmasao-dev <mmasao@gmail.com>
2022-10-28 17:41:17 -0300

    commit script.bat

---
+++ script.bat
@@ -0,0 +1,13 @@
+echo "Criando as imagens .........."
+
+docker build -t mmasao/projeto-backend:1.0 backend/.
+
+docker build -t mmasao/projeto-database:1.0 database/.
+
+echo "Realisando push das imagens ......."
+
+docker push mmasao/projeto-backend:1.0
+docker push mmasao/projeto-database:1.0
+
+
+
