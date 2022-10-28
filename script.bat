1d46d0c1f7cd39c8359bebf787a28723acf53701
mmasao-dev <mmasao@gmail.com>
2022-10-28 17:51:20 -0300

    criando serviço cluste k8

--- script.bat
+++ script.bat
@@ -9,5 +9,11 @@ echo "Realisando push das imagens ......."
 docker push mmasao/projeto-backend:1.0
 docker push mmasao/projeto-database:1.0
 
+echo "Criando serviços do cluster kubernetes"
+
+kubctl apply -f ./services.yml
+
+
+
 
 
